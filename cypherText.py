52cc4468fce3dfce416928c284c1be7dc6c017af
India <69780712+IndiaAce@users.noreply.github.com>
2022-07-11 09:46:38 -0600

    initial commit

---
+++ cypherText.py
@@ -0,0 +1,40 @@
+MAPPING = {
+    'A': 'N',
+    'B': 'O',
+    'C': 'P',
+    'D': 'Q',
+    'E': 'R',
+    'F': 'S',
+    'G': 'T',
+    'H': 'U',
+    'I': 'V',
+    'J': 'W',
+    'K': 'X',
+    'L': 'Y',
+    'M': 'Z',
+    'N': 'A',
+    'O': 'B',
+    'P': 'C',
+    'Q': 'D',
+    'R': 'E',
+    'S': 'F',
+    'T': 'G',
+    'U': 'H',
+    'V': 'I',
+    'W': 'J',
+    'X': 'K',
+    'Y': 'L',
+    'Z': 'M',
+}
+
+def cipher(original):
+    text = " "
+    for letter in original:
+        letter = letter.upper()
+        new_letter = MAPPING[letter]
+        text = text + new_letter
+    return text
+
+text = input("Enter text to be ciphered: ")
+result = cipher(text)
+print(result)    
